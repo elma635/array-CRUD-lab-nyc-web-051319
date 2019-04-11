e8575efc35ff4f2cd05f546f0c182282eb9e3e6a
Elma Rahman <elmarahman635@gmail.com>
2019-04-11 20:59:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-"chocolate" == my_array.shift
+"chocolate" = my_array.shift
+puts chocolate
 end
 
 def retrieve_element_from_index(array, index_number)
